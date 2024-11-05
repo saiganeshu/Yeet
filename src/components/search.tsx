a78c1260ba3dadc175c294fa02dd2683521ef5ca
Deepu714 <deepika.d@worthitcs.com>
2024-11-05 18:15:51 +0530

    search.tsx-changed inlinecss

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,12 +1,14 @@
 import { Text, View } from "react-native";
 import Header from "./header";
+import React from "react";
 
  export default function Search(){
     return(
         
         <View>
             <Header/>
-            <View style={{height: 50, width: '100%', justifyContent: 'center', alignItems: 'center'}}>
+            {/* <View style={{height: 50, width: '100%', justifyContent: 'center', alignItems: 'center'}}> */}
+            <View className="h-50 w-100 justify-center items-center">
                 <Text style={{fontWeight: 'bold', color: '#000000'}}>
                     Search Component
                 </Text>
