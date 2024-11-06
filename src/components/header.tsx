2eeb42f2d292569e0f1e0a39556e61cdadbfa4f0
Saravana213 <saravanan.ngl@gmail.com>
2024-11-06 16:53:43 +0530

    header.tsx-changed inlinecss

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { Image, Text,View } from "react-native";
+import { Image, View } from "react-native";
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Icon from 'react-native-vector-icons/Ionicons';
-const Header = ()=>{
-return(
-    <View style={{ height: 60,width:'100%',flexDirection: "row", alignItems: "center", justifyContent: "space-between" ,borderBottomWidth:1,borderColor: '#808080'}}>
-  <View style={{paddingLeft:10}}>
-    <Image source={require('../assets/images/logo-mobile-light.png')} style={{ width: 180, height: 50}} />
-  </View>
-  <View style={{display:"flex",flexDirection:'row',justifyContent:'space-evenly',width:100}}>
-   <FontAwesome name="user" size={24} color="#ce375a" />
-   <Icon name="notifications-sharp" size={24} color="#ce375a" />
-  </View>
-</View>
 
-
-)
+const Header = () => {
+  return (
+    <View className="h-16 w-full flex-row items-center justify-between border-b border-gray-400">
+      <View className="pl-2.5">
+        <Image source={require('../assets/images/logo-mobile-light.png')} className="w-44 h-12" />
+      </View>
+      <View className="flex flex-row justify-evenly w-24">
+        <FontAwesome name="user" size={24} color="#ce375a" />
+        <Icon name="notifications-sharp" size={24} color="#ce375a" />
+      </View>
+    </View>
+  );
 }
+
 export default Header;
