8fa706931c401b4500a57baf4ac0f0c6ebdcc829
Saravana213 <saravanan.ngl@gmail.com>
2024-11-06 10:51:02 +0530

    footer.tsx-changed textinlinecss

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,11 @@
 import { Text } from "react-native";
 
-const Footer = ()=>{
-return(
-    <Text>Footer component</Text>
-)
+const Footer = () => {
+    return (
+        <Text className="text-center text-gray-600">
+            Footer component
+        </Text>
+    );
 }
+
 export default Footer;
